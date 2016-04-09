ad308c8bd2f711f7eb82f0abd0acfe44de6139d9
zateyev <zhasulan.ateyev@gmail.com>
2016-04-09 18:57:45 +0600

    Initial commit

---
+++ src/main/java/com/epam/webtest/config/SpringSecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.epam.webtest.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/com/epam/webtest/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.epam.webtest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.epam.webtest.*")
+@Import({WebSecurityConfig.class})
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/WEB-INF/pages/**").addResourceLocations("/pages/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+}
